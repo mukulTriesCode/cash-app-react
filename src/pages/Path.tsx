06d47d60c5db83567069b5647ac4c98dbed3a53d
Mukul Sharma <sharma.mukul@rwit.io>
2024-10-03 19:01:14 +0530

    Added lenis and path animation

--- src/pages/Path.tsx
+++ src/pages/Path.tsx
@@ -3,7 +3,9 @@ import LinePath from "../components/LinePath";
 const Path = () => {
   return (
     <div>
+      <div className="h-[800px] w-52 mx-auto bg-white"></div>
       <LinePath />
+      <div className="h-[800px] w-52 mx-auto bg-white"></div>
     </div>
   );
 };
