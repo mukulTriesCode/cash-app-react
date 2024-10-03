6392385bd2c8ae9946369402228fecce19d93a8f
Mukul Sharma <sharma.mukul@rwit.io>
2024-10-03 13:59:37 +0530

    Added BarChart

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import CashBoard from "./CashBoard";
 import CategorySlider from "./CategorySlider";
+import BarChart from "./BarChart";
 
 const Dashboard: React.FC = () => {
   return (
     <div className="ms-[88px] grid grid-cols-3 gap-8 p-7">
       <CashBoard />
+      <BarChart />
       <CategorySlider />
+      <BarChart />
     </div>
   );
 };
