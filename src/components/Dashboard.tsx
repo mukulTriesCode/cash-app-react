d25c400c7869f8b078bba2059b3896109503b2f2
Mukul Sharma <sharma.mukul@rwit.io>
2024-10-03 13:56:09 +0530

    Added category slider

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CashBoard from "./CashBoard";
+import CategorySlider from "./CategorySlider";
+
 const Dashboard: React.FC = () => {
   return (
     <div className="ms-[88px] grid grid-cols-3 gap-8 p-7">
       <CashBoard />
+      <CategorySlider />
     </div>
   );
 };
