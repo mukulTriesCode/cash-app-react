9fcd1ef855e8310b5810148af95ec04083535d8f
Mukul Sharma <sharma.mukul@rwit.io>
2024-10-04 17:57:55 +0530

    Added linechart

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import CashBoard from "./CashBoard";
 import CategorySlider from "./CategorySlider";
 import BarChart from "./BarChart";
+import TransactionHistory from "./TransactionHistory";
 
 const Dashboard: React.FC = () => {
   return (
     <div className="ms-[88px] grid grid-cols-3 gap-8 p-7">
       <CashBoard />
-      <BarChart />
+      <BarChart chartType="bar" />
       <CategorySlider />
-      <BarChart />
+      <TransactionHistory />
     </div>
   );
 };
