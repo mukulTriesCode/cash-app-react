8d3c35d2450129dee35a5f7693af764ebc5c3a41
Mukul Sharma <sharma.mukul@rwit.io>
2024-10-07 13:46:13 +0530

    Added chatbot

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,22 @@ import Sidebar from "../components/Sidebar";
 import Dashboard from "../components/Dashboard";
 import background from "../assets/Dashboard-min (1).webp";
 import Navbar from "../components/Navbar";
+import ChatBot from "react-chatbotify";
 
 const Home = () => {
+  const flow = {
+    start: {
+      message: "Hey! Keep within the character limit!",
+      path: "loop",
+    },
+    loop: {
+      message: "Seems like you are within the limit, great job!",
+      path: "loop",
+    },
+  };
   return (
     <div>
+      <ChatBot flow={flow} />
       <main className="relative">
         <div className="absolute z-[-1] top-0 left-0 w-full h-screen opacity-10">
           <img
