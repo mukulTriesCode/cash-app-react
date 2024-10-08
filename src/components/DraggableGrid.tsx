1949d6b8d6f3bec6d8af17e72b2341c0ba7693d6
baDJax <mukulsharma262002@gmail.com>
2024-10-08 18:16:22 +0530

    Added grid page

--- src/components/DraggableGrid.tsx
+++ src/components/DraggableGrid.tsx
@@ -3,16 +3,24 @@ import GridLayout from "react-grid-layout";
 
 const DraggableGrid: React.FC = () => {
   const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2},
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+    { i: "a", x: 0, y: 0, w: 6, h: 4},
+    { i: "b", x: 7, y: 0, w: 6, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 3, y: 0, w: 3, h: 2 },
+    { i: "d", x: 0, y: 4, w: 3, h: 3},
+    { i: "e", x: 3, y: 4, w: 3, h: 3},
+    { i: "f", x: 6, y: 4, w: 3, h: 3},
+    { i: "g", x: 9, y: 4, w: 3, h: 3},
   ];
   return (
     <section>
       <GridLayout layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div className="bg-pink-200 p-5" key="a">a</div>
-        <div className="bg-pink-200 p-5" key="b">b</div>
-        <div className="bg-pink-200 p-5" key="c">c</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="a">a</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="b">b</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="c">c</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="d">d</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="e">e</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="f">f</div>
+        <div className="rounded-lg flex justify-center items-center bg-gray-400/50 hover:bg-gray-400/70 transition-colors p-5" key="g">g</div>
       </GridLayout>
     </section>
   );
---
+++ src/pages/Grid.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DraggableGrid from "../components/DraggableGrid";
+
+const Grid: React.FC = () => {
+  return (
+    <div className="container">
+      <DraggableGrid />
+    </div>
+  );
+};
+
+export default Grid;
