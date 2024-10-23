ae542220358b0b81dc3daf3db256b2bafa5028f8
baDJax <mukulsharma262002@gmail.com>
2024-10-23 13:43:29 +0530

    Configured redux provider in layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import { ReactLenis } from "lenis/react";
 import React from "react";
 import ValProvider from "./ValProvider";
+import { Provider } from "react-redux";
+import { store } from "../store";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <ReactLenis root>
-      <ValProvider>{children}</ValProvider>
+      <Provider store={store}>
+        <ValProvider>{children}</ValProvider>
+      </Provider>
     </ReactLenis>
   );
 };
