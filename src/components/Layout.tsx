139a57de20f4bfa304aedc1fd36e28516915e1d5
baDJax <mukulsharma262002@gmail.com>
2024-10-24 18:03:47 +0530

    Added persist gate wrapper in layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,16 @@ import { ReactLenis } from "lenis/react";
 import React from "react";
 import ValProvider from "./ValProvider";
 import { Provider } from "react-redux";
-import { store } from "../store";
+import { persistor, store } from "../store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <ReactLenis root>
       <Provider store={store}>
-        <ValProvider>{children}</ValProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <ValProvider>{children}</ValProvider>
+        </PersistGate>
       </Provider>
     </ReactLenis>
   );
