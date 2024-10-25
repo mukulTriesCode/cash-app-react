0a519fda24a30f2d5b6bdf597862053aa7d1dc3b
baDJax <mukulsharma262002@gmail.com>
2024-10-25 14:51:09 +0530

    Updated cashcounterstate

--- src/features/cashCountSlice/index.ts
+++ src/features/cashCountSlice/index.ts
@@ -3,34 +3,30 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface CashCounterState {
   value: number;
+  notes?: string;
 }
 
 const initialState: CashCounterState = {
   value: 0,
+  notes: "",
 };
 
 export const cashCountSlice = createSlice({
   name: "cashCounter",
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    incrementByAmount: (state, action: PayloadAction<{ amount: number; notes?: string }>) => {
+      state.value += action.payload.amount;
+      state.notes = action.payload.notes || state.notes;
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    decrementByAmount: (state, action: PayloadAction<{ amount: number; notes?: string }>) => {
+      state.value -= action.payload.amount;
+      state.notes = action.payload.notes || state.notes;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } =
-  cashCountSlice.actions;
+export const { incrementByAmount, decrementByAmount } = cashCountSlice.actions;
 
 export default cashCountSlice.reducer;
