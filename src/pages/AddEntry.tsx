263fb25615db69b35a8eda54af3cce229b691c7c
baDJax <mukulsharma262002@gmail.com>
2024-10-28 10:42:23 +0530

    Updated add entry page

--- src/pages/AddEntry.tsx
+++ src/pages/AddEntry.tsx
@@ -1,12 +1,18 @@
 import AddAmount from "@/components/AddAmount";
 import TotalData from "@/components/TotalData";
+import TransactionList from "@/components/TransactionList";
 import React from "react";
 
 const AddEntry: React.FC = () => {
   return (
-    <div className="p-7">
-      <TotalData />
-      <AddAmount />
+    <div className="p-7 grid grid-cols-4 gap-7">
+      <div className="col-span-1">
+        <TotalData />
+        <AddAmount />
+      </div>
+      <div className="col-span-3">
+        <TransactionList />
+      </div>
     </div>
   );
 };
