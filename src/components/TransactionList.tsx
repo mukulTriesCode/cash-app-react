12456840e3af645c271a4170f068d47c900ebff0
baDJax <mukulsharma262002@gmail.com>
2024-10-28 10:42:10 +0530

    Added transaction list component

---
+++ src/components/TransactionList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TransactionList: React.FC = () => {
+  return (
+    <div className="p-4 h-full border border-white/15 rounded-md bg-[#131313]">
+      TransactionList
+    </div>
+  );
+};
+
+export default TransactionList;
