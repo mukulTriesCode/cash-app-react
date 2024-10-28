e9e6ec765ad6c3b21b11c737a59692d568dda534
baDJax <mukulsharma262002@gmail.com>
2024-10-28 15:57:07 +0530

    Updated amount variable in transaction list

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 import { useSelector } from "react-redux";
 
 const TransactionList: React.FC = () => {
-  const totalCash = useSelector((state: RootState) => state?.root?.value);
+  const totalCash = useSelector((state: RootState) => state?.root?.totalAmount);
 
   return (
     <div className="p-4 h-full border border-white/15 rounded-md bg-[#131313] text-base">
