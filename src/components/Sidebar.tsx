1b4703a76bfb17950d764e900eefc1ac294df166
baDJax <mukulsharma262002@gmail.com>
2024-11-06 11:56:07 +0530

    Updated fixed sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,27 +16,30 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className="flex">
+      <div className={`fixed z-[1000] flex-1 p-4 py-6 ml-0`}>
+        <div className="ml-auto">
+          <button
+            id="breadcrumb"
+            aria-label="breadcrumb"
+            className=" transition-all bg-purple-shade hover:bg-purple-shade/90 font-bold py-3 px-4 rounded-lg"
+            onClick={() => {
+              if (toggleVal) {
+                toggleVal();
+              }
+            }}
+          >
+            {val ? <CrossSVG /> : <NavSVG />}
+          </button>
+        </div>
+      </div>
       <div
-        className={`bg-[#131313] fixed z-[1000] border-r-[0.5px] border-white/10 h-screen transition-all duration-300 ${
-          val ? "w-72" : "w-[88px] overflow-hidden"
+        data-lenis-prevent
+        className={`bg-[#131313] fixed z-[999] border-r-[0.5px]  border-white/10 h-screen transition-all duration-300 ${
+          val
+            ? "w-screen overflow-y-scroll "
+            : "w-[88px] overflow-hidden -translate-x-full"
         }`}
       >
-        <div className={`flex-1 p-4 py-6 ml-0 z-10`}>
-          <div className="ml-auto">
-            <button
-              id="breadcrumb"
-              aria-label="breadcrumb"
-              className="transition-all bg-purple-shade hover:bg-purple-shade/90 font-bold py-3 px-4 rounded-lg"
-              onClick={() => {
-                if (toggleVal) {
-                  toggleVal();
-                }
-              }}
-            >
-              {val ? <CrossSVG /> : <NavSVG />}
-            </button>
-          </div>
-        </div>
         <div className="flex flex-col flex-nowrap items-center px-3">
           {navLinks.map((link, i) => (
             <Link
