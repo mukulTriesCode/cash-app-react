82136cb40c917d942b3879da5fc38419fa97c826
Mukul Sharma <mukulsharma262002@gmail.com>
2025-01-13 23:32:06 +0530

    Created profile store

---
+++ src/store/profileStore/index.ts
@@ -0,0 +1,38 @@
+import { configureStore } from "@reduxjs/toolkit";
+import profileData from "../../features/profileData";
+import {
+  persistStore,
+  persistReducer,
+  REGISTER,
+  PURGE,
+  PERSIST,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, profileData);
+
+export const store = configureStore({
+  reducer: {
+    root: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type ProfileRootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
