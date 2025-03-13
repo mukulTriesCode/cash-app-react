dd2a774e70976545c3283c62c264c905a8e7c418
Mukul <mukulsharma262002@gmail.com>
2025-03-13 22:48:15 +0530

    Add VerticalSwiper component for vertical swiping of transaction entries with lazy loading support

---
+++ src/components/swiper/VerticalSwiper.tsx
@@ -0,0 +1,39 @@
+import React, { lazy } from 'react';
+import { Virtual } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Entry } from "@/features/cashCountSlice";
+import { SWIPER_CONFIG, MAX_ENTRIES } from './SwiperConfig';
+import 'swiper/css/virtual';
+import SwiperItemSkeleton from './SwiperItemSkeleton';
+
+const TransactionItem = lazy(() => import('../TransactionItem'));
+
+interface VerticalSwiperProps {
+  entries: Entry[];
+}
+
+const VerticalSwiper: React.FC<VerticalSwiperProps> = ({ entries }) => {
+  const displayEntries = entries.slice(0, MAX_ENTRIES);
+
+  return (
+    <Swiper
+      data-lenis-prevent
+      modules={[Virtual]}
+      {...SWIPER_CONFIG}
+    >
+      {displayEntries.map((entry, index) => (
+        <SwiperSlide key={entry?.id} virtualIndex={index} className='h-fit'>
+          <React.Suspense fallback={<SwiperItemSkeleton />}>
+            <TransactionItem
+              entry={entry}
+              isLast={index === displayEntries.length - 1}
+              index={index}
+            />
+          </React.Suspense>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default VerticalSwiper;
