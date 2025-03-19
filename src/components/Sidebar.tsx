3ff8e664099e266b397fb2d8d23d5fd8951716a0
Mukul <mukulsharma262002@gmail.com>
2025-03-19 23:28:56 +0530

    Enhance Sidebar component by adding a search input for mobile users and improving layout responsiveness with adjusted padding and styles.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import ValContext from "../helpers/Context/ValContext";
-import { CrossSVG, NavSVG } from "@/lib/Svgs";
+import { CrossSVG, NavSVG, SearchSVG } from "@/lib/Svgs";
 import { GoHomeFill as HomeIcon } from "react-icons/go";
 import { BiSolidCategoryAlt as CategoryIcon } from "react-icons/bi";
 import { IoMdAddCircle as AddEntryIcon } from "react-icons/io";
 import { CgProfile as ProfileIcon } from "react-icons/cg";
 import { LuLogIn as Login } from "react-icons/lu";
+import useMobile from "@/hooks/useMobile";
 
 const Sidebar: React.FC = () => {
   const context = useContext(ValContext);
   const { val, toggleVal } = context || {};
+  const isMobile = useMobile(640);
 
   const navLinks = [
     { path: "/", label: "Home", icon: <HomeIcon /> },
@@ -28,7 +30,7 @@ const Sidebar: React.FC = () => {
           val ? "w-full xs:w-72" : "w-0 xs:w-[88px] xs:overflow-hidden"
         }`}
       >
-        <div className={`absolute flex-1 p-4 py-6 ml-0 z-10`}>
+        <div className={`absolute flex-1 p-4 xs:py-6 ml-0 z-10`}>
           <div className="ml-auto">
             <button
               id="breadcrumb"
@@ -67,6 +69,20 @@ const Sidebar: React.FC = () => {
               </span>
             </Link>
           ))}
+          {isMobile && (
+            <div className="w-full max-w-[332px] self-start p-4">
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="w-full p-4 py-3 pr-10 rounded-lg bg-[#a2a3a446] text-white/75 outline-none focus:ring-2 focus:ring-white/50"
+                />
+                <span className="cursor-pointer">
+                  <SearchSVG />
+                </span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
