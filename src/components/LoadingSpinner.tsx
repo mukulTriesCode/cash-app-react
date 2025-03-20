be3a5e89119898773f32d794de13cf9799a6f1ef
Mukul <mukulsharma262002@gmail.com>
2025-03-20 22:35:52 +0530

    Updated loading spinner UI

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadingSpinner: React.FC = () => (
-  <div className="flex h-full w-full justify-center items-center">
+  <div className="flex h-[calc(100vh-81px)] xs:h-[calc(100vh-95px)] w-full justify-center items-center">
     <div className="ping">
     </div>
   </div>
