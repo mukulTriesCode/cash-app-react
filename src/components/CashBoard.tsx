e63365623a8f8eea5931ad875e9b3fa260e42e0d
Mukul <mukulsharma262002@gmail.com>
2025-03-23 18:36:59 +0530

    Updated font size for cashboard heading

--- src/components/CashBoard.tsx
+++ src/components/CashBoard.tsx
@@ -31,7 +31,7 @@ const CashBoard: React.FC = () => {
       )}
       <div className="w-full flex-none xl:flex-auto flex flex-col tab:max-w-[450px]">
         <div className="flex items-center justify-between">
-          <h2 className="text-3xl font-bold">Records</h2>
+          <h2 className="text-2xl xs:text-3xl font-bold">Records</h2>
           <div className="flex items-center">
             <HeartSVG />
           </div>
