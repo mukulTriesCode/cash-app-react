cc2a2cfbf90ce806cbf6a1b4697b36303c5c5267
Mukul <mukulsharma262002@gmail.com>
2025-03-23 18:39:29 +0530

    Added scrolling for transaction list to improve responsive view

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -7,41 +7,43 @@ const TransactionList: React.FC = () => {
   const totalAmount = root?.totalAmount;
   const entries = root?.entries || [];
   return (
-    <div className="p-4 h-full border border-white/15 rounded-md bg-[#131313] text-base">
-      <TList className="text-white/80 text-sm pt-0 border-white/50">
-        <li className="col-span-2">Transaction ID</li>
-        <li className="col-span-4">Note</li>
-        <li className="col-span-2">Date</li>
-        <li className="col-span-2">Category</li>
-        <li className="col-span-2">Amount</li>
-      </TList>
-      <ul data-lenis-prevent className="h-[590px] overflow-y-auto">
-        {entries?.map((val) => (
-          <li key={val?.id}>
-            <TList>
-              <li className="col-span-2">{val?.id}</li>
-              <li className="col-span-4">{val?.notes ? val?.notes : "--"}</li>
-              <li className="col-span-2">
-                {val?.date ? new Date(val?.date).toLocaleDateString() : "--"}
-              </li>
-              <li className="col-span-2 capitalize">
-                {val?.category ? val?.category : "--"}
-              </li>
-              <li
-                className={`col-span-2 ${
-                  val?.isCashIn ? "text-green-500" : "text-red-500"
-                }`}
-              >
-                ₹ {val?.amount ? val?.amount : "--"}
-              </li>
-            </TList>
-          </li>
-        ))}
-      </ul>
-      <TList className="text-white/80 py-3 bg-[#232323] border-none rounded-md">
-        <li className="">Total</li>
-        <li className="col-start-11 col-end-12">₹ {totalAmount}</li>
-      </TList>
+    <div className="w-[calc(100vw-32px)] xs:w-full overflow-auto p-4 h-full border border-white/15 rounded-md bg-[#131313] text-base">
+      <div className="min-w-[640px] xs:min-w-full w-full">
+        <TList className="text-white/80 text-sm pt-0 border-white/50">
+          <li className="col-span-2">Transaction ID</li>
+          <li className="col-span-4">Note</li>
+          <li className="col-span-2">Date</li>
+          <li className="col-span-2">Category</li>
+          <li className="col-span-2">Amount</li>
+        </TList>
+        <ul data-lenis-prevent className="h-[590px] overflow-y-auto">
+          {entries?.map((val) => (
+            <li key={val?.id}>
+              <TList>
+                <li className="col-span-2">{val?.id}</li>
+                <li className="col-span-4">{val?.notes ? val?.notes : "--"}</li>
+                <li className="col-span-2">
+                  {val?.date ? new Date(val?.date).toLocaleDateString() : "--"}
+                </li>
+                <li className="col-span-2 capitalize">
+                  {val?.category ? val?.category : "--"}
+                </li>
+                <li
+                  className={`col-span-2 ${
+                    val?.isCashIn ? "text-green-500" : "text-red-500"
+                  }`}
+                >
+                  ₹ {val?.amount ? val?.amount : "--"}
+                </li>
+              </TList>
+            </li>
+          ))}
+        </ul>
+        <TList className="text-white/80 py-3 bg-[#232323] border-none rounded-md">
+          <li className="">Total</li>
+          <li className="col-start-11 col-end-12">₹ {totalAmount}</li>
+        </TList>
+      </div>
     </div>
   );
 };
