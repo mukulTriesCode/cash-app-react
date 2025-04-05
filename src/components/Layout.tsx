09ea44c31e486fc29292f47fd423e2cf6758cd88
Mukul <mukulsharma262002@gmail.com>
2025-04-05 22:53:08 +0530

    Implement conditional rendering of Sidebar and Navbar based on route protection in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,21 +4,34 @@ import ValProvider from "./ValProvider";
 import { Provider } from "react-redux";
 import { persistor, store } from "../store";
 import { PersistGate } from "redux-persist/integration/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import background from "../assets/Dashboard-min.webp";
 import useMobile from "@/hooks/useMobile";
+import { routes } from "../routes";
 
 const Layout: React.FC = () => {
   const isMobile = useMobile(640);
+  const location = useLocation();
+
+  const isProtectedRoute = routes.find(
+    (route) => route.path === location.pathname
+  )?.protected;
+
   return (
     <ReactLenis root>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ValProvider>
-            <Sidebar />
-            <div className="xs:ms-[88px] relative">
+            {isProtectedRoute && <Sidebar />}
+            <div
+              className={
+                isProtectedRoute
+                  ? "xs:ms-[88px] relative"
+                  : "relative grid grid-cols-5"
+              }
+            >
               {!isMobile && (
                 <div className="absolute z-[-1] top-0 left-0 w-full min-h-screen h-full opacity-10">
                   <img
@@ -35,9 +48,23 @@ const Layout: React.FC = () => {
                   />
                 </div>
               )}
-              <Navbar />
-              <div className="pt-[81px] xs:pt-[95px]">
-                <Outlet />
+              {isProtectedRoute ? (
+                <Navbar />
+              ) : (
+                <div className="col-span-3 bg-blue-600"></div>
+              )}
+              <div
+                className={`${
+                  isProtectedRoute ? "pt-[81px] xs:pt-[95px]" : "col-span-2"
+                }`}
+              >
+                {isProtectedRoute ? (
+                  <Outlet />
+                ) : (
+                  <div className="h-screen w-full flex justify-center items-center px-8">
+                    <Outlet />
+                  </div>
+                )}
               </div>
             </div>
           </ValProvider>
