b97a0a39128d5d784b33469e6e5d0ac70e9d8699
Mukul <mukulsharma262002@gmail.com>
2025-04-06 23:39:21 +0530

    Refactor Layout component to comment out mobile detection and adjust layout classes for responsiveness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,11 +8,11 @@ import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import background from "../assets/Dashboard-min.webp";
-import useMobile from "@/hooks/useMobile";
+// import useMobile from "@/hooks/useMobile";
 import { routes } from "../routes";
 
 const Layout: React.FC = () => {
-  const isMobile = useMobile(640);
+  // const isMobile = useMobile(640);
   const location = useLocation();
 
   const isProtectedRoute = routes.find(
@@ -32,7 +32,7 @@ const Layout: React.FC = () => {
                   : "relative grid grid-cols-5"
               }
             >
-              {!isMobile && (
+              {/* {!isMobile && ( */}
                 <div className="absolute z-[-1] top-0 left-0 w-full min-h-screen h-full opacity-10">
                   <img
                     className="w-full h-full object-cover"
@@ -47,15 +47,15 @@ const Layout: React.FC = () => {
                     height="1080"
                   />
                 </div>
-              )}
+              {/* )} */}
               {isProtectedRoute ? (
                 <Navbar />
               ) : (
-                <div className="col-span-3 bg-blue-600"></div>
+                <div className="hidden sm:block col-span-3 bg-blue-600"></div>
               )}
               <div
                 className={`${
-                  isProtectedRoute ? "pt-[81px] xs:pt-[95px]" : "col-span-2"
+                  isProtectedRoute ? "pt-[81px] xs:pt-[95px]" : "col-span-5 sm:col-span-2"
                 }`}
               >
                 {isProtectedRoute ? (
