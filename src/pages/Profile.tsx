3f238573f347508fe44b2559612837e5a9b06678
Mukul <mukulsharma262002@gmail.com>
2025-05-13 23:42:42 +0530

    Update profile details rendering to use data object

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -24,10 +24,9 @@ const Profile: React.FC = () => {
     <div className="p-4">
       <div className="max-w-[500px] p-4 mx-auto border flex flex-col gap-4">
         <h4 className="text-3xl">Your Profile</h4>
-        <p className="text-xl">ID: {id}</p>
-        <p className="text-xl">Username: {username}</p>
-        <p className="text-xl">Email: {email}</p>
-        <p className="text-xl">Password: {password}</p>
+        <p className="text-xl">ID: {data?._id}</p>
+        {/* <p className="text-xl">Username: {username}</p> */}
+        <p className="text-xl">Email: {data?.email}</p>
       </div>
     </div>
   );
