926f3ba63047469df6763fc3a914bce71e66ce12
Mukul <mukulsharma262002@gmail.com>
2025-05-13 23:42:02 +0530

    Fetched profile data in profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { RootState } from "@/store";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+// import { RootState } from "@/store";
+// import { useSelector } from "react-redux";
 
 const Profile: React.FC = () => {
-  const profileData = useSelector((state: RootState) => state.profile);
-  const { id, username, email, password } = profileData || {};
+  // const profileData = useSelector((state: RootState) => state.profile);
+  // const { id, username, email, password } = profileData || {};
+  const [data, setData] = useState<{ _id: string; email: string } | null>(null);
+  const fetchData = async () => {
+    const res = await fetch("/api/user/profile", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    });
+    const data = await res.json();
+    setData(data.user);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="p-4">
       <div className="max-w-[500px] p-4 mx-auto border flex flex-col gap-4">
