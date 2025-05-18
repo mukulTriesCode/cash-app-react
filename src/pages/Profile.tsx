77611f25bfa31220cb8813c547f62f31d1c28c9c
Mukul <mukulsharma262002@gmail.com>
2025-05-18 19:16:44 +0530

    Replaced manual fetch with useGetUserQuery hook

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,35 +1,25 @@
-import { getToken } from "@/lib/utils";
-import React, { useEffect, useState } from "react";
+import { useGetUserQuery } from "@/services/profileService";
+import React from "react";
 // import { RootState } from "@/store";
 // import { useSelector } from "react-redux";
 
 const Profile: React.FC = () => {
   // const profileData = useSelector((state: RootState) => state.profile);
   // const { id, username, email, password } = profileData || {};
-  const [data, setData] = useState<{ _id: string; email: string } | null>(null);
-  const token = getToken();
-  const fetchData = async () => {
-    const res = await fetch("/api/user/profile", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const data = await res.json();
-    setData(data.user);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { data, isLoading } = useGetUserQuery("");
+  const user = data?.user;
   return (
     <div className="p-4">
-      <div className="max-w-[500px] p-4 mx-auto border flex flex-col gap-4">
-        <h4 className="text-3xl">Your Profile</h4>
-        <p className="text-xl">ID: {data?._id}</p>
-        {/* <p className="text-xl">Username: {username}</p> */}
-        <p className="text-xl">Email: {data?.email}</p>
-      </div>
+      {isLoading ? (
+        <></>
+      ) : (
+        <div className="max-w-[500px] p-4 mx-auto border flex flex-col gap-4">
+          <h4 className="text-3xl">Your Profile</h4>
+          <p className="text-xl">ID: {user?._id}</p>
+          {/* <p className="text-xl">Username: {username}</p> */}
+          <p className="text-xl">Email: {user?.email}</p>
+        </div>
+      )}
     </div>
   );
 };
