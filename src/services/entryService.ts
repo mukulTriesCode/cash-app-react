bf25721978be1034ed1507277f61885da8a60c35
Mukul <mukulsharma262002@gmail.com>
2025-05-19 23:22:44 +0530

    Added addEntry query for creating new entries

---
+++ src/services/entryService.ts
@@ -0,0 +1,26 @@
+import { getToken } from "@/lib/utils";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const token = getToken();
+
+export const addEntryApi = createApi({
+  reducerPath: "addEntryApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  endpoints: (build) => ({
+    addEntry: build.mutation({
+      query: (body) => ({
+        url: `/api/entry/add-entry`,
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        },
+        body,
+      }),
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useAddEntryMutation } = addEntryApi;
