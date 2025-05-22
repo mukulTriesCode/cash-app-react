c04b37eedef8603952b71fe9984dd5c433d33658
Mukul <mukulsharma262002@gmail.com>
2025-05-22 23:52:27 +0530

    Updated the token handling in headers using prepareHeaders in userApi

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,20 +1,23 @@
 import { getToken } from "@/lib/utils";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const token = getToken();
-
 export const userApi = createApi({
   reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/",
+    prepareHeaders: (headers) => {
+      const token = getToken();
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (build) => ({
     getUser: build.query({
       query: () => ({
         url: `/api/user/profile`,
         method: "GET",
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          Authorization: `Bearer ${token}`,
-        }
       }),
     }),
   }),
