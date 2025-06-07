e46c2929b16d65cfc560f1dc3cf296e88caf9963
Mukul <mukulsharma262002@gmail.com>
2025-06-07 23:01:42 +0530

    Added data fetching and loading state handling

--- src/pages/AddEntry.tsx
+++ src/pages/AddEntry.tsx
@@ -6,6 +6,14 @@ import { useGetEntriesQuery } from "@/services/entryService";
 import React from "react";
 
 const AddEntry: React.FC = () => {
+  const { data, isLoading } = useGetEntriesQuery("");
+  useLoader(isLoading);
+  const totalAmount = data?.totalAmount;
+  const lastEntry = data?.data?.length > 0 ? {
+    amount: data.data[data.data.length - 1]?.amount,
+    notes: data.data[data.data.length - 1]?.notes
+  } : null;
+
   return (
     <div className="p-4 xs:p-7 grid grid-cols-7 xl:grid-cols-4 gap-4 xs:gap-7">
       <div className="col-span-7 lg:col-span-2 xl:col-span-1">
