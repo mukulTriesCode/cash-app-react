f6dd942c12bc2c7610e9499742e25aa290e149c0
Mukul <mukulsharma262002@gmail.com>
2025-06-07 23:05:59 +0530

    Used entries prop instead of cashEntry for total amount

--- src/components/TotalData.tsx
+++ src/components/TotalData.tsx
@@ -13,7 +13,7 @@ const TotalData: React.FC<Entries> = (entries) => {
     <div className="mb-4 xs:mb-7 w-full p-4 border border-white/15 rounded-md bg-[#131313] flex flex-col gap-3">
       <h2 className="text-2xl font-semibold">Record</h2>
       <div className="text-lg flex flex-col gap-1">
-        <p>Total amount : {cashEntry?.totalAmount}</p>
+        <p>Total amount : {entries?.totalAmount}</p>
         <p>
           Recent Transaction :{" "}
           {cashEntry?.entries.length > 0 && (
