f6e84ee96278084b3331830081488f6b45eb996b
Mukul <mukulsharma262002@gmail.com>
2025-06-07 23:02:20 +0530

    Passed totalAmount and lastEntry as props in TotalData

--- src/pages/AddEntry.tsx
+++ src/pages/AddEntry.tsx
@@ -17,7 +17,7 @@ const AddEntry: React.FC = () => {
   return (
     <div className="p-4 xs:p-7 grid grid-cols-7 xl:grid-cols-4 gap-4 xs:gap-7">
       <div className="col-span-7 lg:col-span-2 xl:col-span-1">
-        <TotalData />
+        <TotalData totalAmount={totalAmount} lastEntry={lastEntry} />
         <AddAmount />
       </div>
       <div className="col-span-7 lg:col-span-5 xl:col-span-3">
